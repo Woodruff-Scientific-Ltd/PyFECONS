cef4d46574cf417eba333f5d1cca24d3942a202e
craastad <craastad@gmail.com>
2024-02-21 17:21:24 +0900

    Update library version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyfecons',
-    version='0.0.1',
+    version='0.0.2',
     author='nTtau Digital LTD',
     author_email='info@nttaudigital.com',
     description='Library for nTtau PyFECONS costing calculations.',
