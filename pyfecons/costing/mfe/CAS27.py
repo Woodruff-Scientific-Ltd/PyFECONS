7cc5357b4f1770e8e2fa3ed939afa11e4854de6e
craastad <craastad@gmail.com>
2024-03-08 12:21:36 +0200

    Fix broken code

--- pyfecons/costing/mfe/CAS27.py
+++ pyfecons/costing/mfe/CAS27.py
@@ -1,21 +1,25 @@
 from pyfecons.inputs import Inputs
 from pyfecons.data import Data
 from pyfecons.enums import BlanketPrimaryCoolant
+
+
 def GenerateData(inputs: Inputs, data: Data, figures: dict):
-    #Cost Category 27 Special Materials
+    # Cost Category 27 Special Materials
 
     # "Assuming the values for c_Flibe, c_Pb, c_Li, and MLM are defined elsewhere in your code"
     #   - the original workbook said this
     # I couldn't find these defined anywhere in the workbook - must ask Alex
 
-
+    materials = inputs.materials
+    # TODO - figure out what the actual value is here
+    MLM = 1
     # Select the coolant and calculate C_27_1
     if inputs.blanket.primary_coolant == BlanketPrimaryCoolant.FLIBE:
-        data.cas27.C271000 = 1000 * 2130 * c_Flibe / 1e6
+        data.cas27.C271000 = 1000 * 2130 * materials.Flibe.c / 1e6
     elif inputs.blanket.primary_coolant == BlanketPrimaryCoolant.LEAD_LITHIUM_PBLI:
         f_6Li = 0.1
         FPCPPFb = 0.9
-        data.cas27.C271000 = (c_Pb * FPCPPFb * MLM * 1000 + c_Li * f_6Li * MLM * 1000) / 1e6
+        data.cas27.C271000 = (materials.Pb.c * FPCPPFb * MLM * 1000 + materials.Li.c * f_6Li * MLM * 1000) / 1e6
     elif inputs.blanket.primary_coolant == BlanketPrimaryCoolant.LITHIUM_LI:
         data.cas27.C271000 = 1000 * 2130 * 50 / 1e6
     elif inputs.blanket.primary_coolant == BlanketPrimaryCoolant.OTHER_EUTECTIC_SALT:
