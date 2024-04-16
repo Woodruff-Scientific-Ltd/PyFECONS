573bb7d740f170508c3231424ba35d1000965c38
craastad <craastad@gmail.com>
2024-04-16 11:24:03 +0200

    Reformat CAS20.

--- pyfecons/costing/mfe/CAS20.py
+++ pyfecons/costing/mfe/CAS20.py
@@ -8,19 +8,19 @@ CAS_200000_TEX = 'CAS200000.tex'
 def GenerateData(inputs: Inputs, data: Data, figures: dict):
     OUT = data.cas20
     # TODO - why are C210000 and C280000 counted twice?
-    OUT.C200000 = M_USD(data.cas21.C210000 + \
-                        data.cas21.C210000 + \
-                        data.cas22.C220000 + \
-                        data.cas23.C230000 + \
-                        data.cas24.C240000 + \
-                        data.cas25.C250000 + \
-                        data.cas26.C260000 + \
-                        data.cas27.C270000 + \
-                        data.cas28.C280000 + \
-                        data.cas28.C280000 + \
+    OUT.C200000 = M_USD(data.cas21.C210000 +
+                        data.cas21.C210000 +
+                        data.cas22.C220000 +
+                        data.cas23.C230000 +
+                        data.cas24.C240000 +
+                        data.cas25.C250000 +
+                        data.cas26.C260000 +
+                        data.cas27.C270000 +
+                        data.cas28.C280000 +
+                        data.cas28.C280000 +
                         data.cas29.C290000)
     OUT.template_file = CAS_200000_TEX
     OUT.replacements = {
-        'C200000': str(data.cas20.C200000) # TODO - C200000 not in the template
+        'C200000': str(data.cas20.C200000)  # TODO - C200000 not in the template
     }
 
