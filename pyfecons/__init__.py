07ad1bd38760388331a49b4afa87c6688a2ab085
craastad <craastad@gmail.com>
2024-04-25 10:38:39 +0100

    Remove redundant class import from package definition

--- pyfecons/__init__.py
+++ pyfecons/__init__.py
@@ -2,7 +2,7 @@ from .data import *
 from .enums import *
 from .inputs import *
 from .materials import *
-from .pyfecons import RunCosting, CreateReportContent, RenderFinalReport, FinalReport
+from .pyfecons import RunCosting, CreateReportContent, RenderFinalReport
 from .units import *
 from .report import *
 
