f6acbf453498c563c455904a4eef262767e6a7b1
craastad <craastad@gmail.com>
2024-05-31 15:58:23 +0300

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pyfecons',
-    version='0.0.27',
+    version='0.0.28',
     author='nTtau Digital LTD',
     author_email='info@nttaudigital.com',
     description='Library for nTtau PyFECONS costing calculations.',
