61acd34b92ade3682c294acf4a46765b0f1f50a1
craastad <craastad@gmail.com>
2024-06-19 14:07:44 +0100

    Reactor MFE cas220101 into an external costing function

--- pyfecons/costing/calculations/volume.py
+++ pyfecons/costing/calculations/volume.py
@@ -8,3 +8,7 @@ def calc_volume_sphere(inner: float, outer: float) -> float:
 
 def calc_volume_ring(height: float, outer: float, inner: float) -> float:
     return Meters3(height * np.pi * (outer ** 2 - inner ** 2))
+
+
+def calc_volume_torus(radius: float, inner: float, outer: float):
+    return Meters3(2 * np.pi * radius * np.pi * (inner + outer) ** 2)
