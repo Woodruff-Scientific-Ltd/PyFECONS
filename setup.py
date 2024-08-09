f5d9aa8d2d88747460dda75113654bfa5517ef7e
craastad <craastad@gmail.com>
2024-08-09 14:09:55 +0200

    Correct version to be next consecutive

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt', encoding='utf-8') as f:
 
 setup(
     name='pyfecons',
-    version='0.0.38',
+    version='0.0.35',
     author='nTtau Digital LTD',
     author_email='info@nttaudigital.com',
     description='Library for nTtau PyFECONS costing calculations.',
