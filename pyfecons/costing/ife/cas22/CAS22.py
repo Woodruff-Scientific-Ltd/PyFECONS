96f1e8650f8c128c1555be49b7719bdd00b46123
craastad <craastad@gmail.com>
2024-10-20 16:28:43 +0900

    Implement CAS 22.1.19 scheduled replacement costs. Update cas22 total costs to include 22.1.8 and 22.1.9. Create function for 22.1 total costs. Update shared method names.

--- pyfecons/costing/ife/cas22/CAS22.py
+++ pyfecons/costing/ife/cas22/CAS22.py
@@ -1,4 +1,4 @@
-from pyfecons.costing.calculations.cas22.cas22_reactor_plant_equipment_total import compute_total_costs, \
+from pyfecons.costing.calculations.cas22.cas22_reactor_plant_equipment_total import compute_cas22_total_costs, \
     compute_replacements
 from pyfecons.data import Data, TemplateProvider
 
@@ -6,7 +6,7 @@ from pyfecons.data import Data, TemplateProvider
 def cas_2200_reactor_plant_equipment_total(data: Data) -> TemplateProvider:
     # Reactor Plant Equipment (RPE) total
     OUT = data.cas22
-    OUT = compute_total_costs(OUT, data)
+    OUT = compute_cas22_total_costs(OUT, data)
     OUT.template_file = 'CAS220000.tex'
     OUT.replacements = compute_replacements(data)
     return OUT
