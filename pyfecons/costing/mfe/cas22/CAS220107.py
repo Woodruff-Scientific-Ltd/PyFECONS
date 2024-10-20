1d270c626e5ca59adb4f2bbb59f19ab1d691935a
craastad <craastad@gmail.com>
2024-10-20 14:26:26 +0900

    Add eur_to_usd conversion. Update MFE power supplies cost calculation.

--- pyfecons/costing/mfe/cas22/CAS220107.py
+++ pyfecons/costing/mfe/cas22/CAS220107.py
@@ -1,3 +1,4 @@
+from pyfecons.costing.calculations.conversions import eur_to_usd
 from pyfecons.data import Data, TemplateProvider
 from pyfecons.inputs import Inputs
 from pyfecons.units import M_USD
@@ -11,9 +12,10 @@ def cas_220107_power_supplies(inputs: Inputs, data: Data) -> TemplateProvider:
     OUT.C22010701 = M_USD(data.power_table.p_coils * IN.cost_per_watt)
 
     # Scaled relative to ITER for a 500MW fusion power system
-    # assuming 1kIUA equals $2 M #cost in kIUA
-    # TODO where does 269.6 come from?
-    OUT.C22010702 = M_USD(269.6 * inputs.basic.p_nrl / 500 * IN.learning_credit * 2)
+    # TODO where does 1552.24 and 269.6 come from?
+    # kIUA exchange rate in USD
+    kIUA = eur_to_usd(1552.24*1e-3)
+    OUT.C22010702 = M_USD(269.6 * inputs.basic.p_nrl / 500 * IN.learning_credit * kIUA)
     OUT.C220107 = M_USD(OUT.C22010701 + OUT.C22010702)
 
     OUT.template_file = "CAS220107_MFE.tex"
