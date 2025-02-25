d3684b1528536a42ad7d23584bf6935740d507a7
craastad <431915+craastad@users.noreply.github.com>
2025-02-25 13:24:17 +0700

    Use shared CAS21 for mfe and ife.

--- pyfecons/costing/calculations/cas21_buildings.py
+++ pyfecons/costing/calculations/cas21_buildings.py
@@ -1,14 +1,15 @@
 from pyfecons.costing.calculations.conversions import k_to_m_usd
-from pyfecons.enums import FuelType
+from pyfecons.enums import FuelType, ReactorType
 from pyfecons.inputs.all_inputs import AllInputs
 from pyfecons.data import Data
 from pyfecons.report import TemplateProvider
 from pyfecons.units import M_USD
 
 
-def cas_21(inputs: AllInputs, data: Data) -> TemplateProvider:
+def cas_21_building_costs(inputs: AllInputs, data: Data) -> TemplateProvider:
     # Cost Category 21: Buildings
     OUT = data.cas21
+    basic = inputs.basic
     p_et = data.power_table.p_et
 
     # Buildings come out to be 470$/kW gross, so if we are looking at a gross power of 1GW, we have:
@@ -17,7 +18,7 @@ def cas_21(inputs: AllInputs, data: Data) -> TemplateProvider:
     # [2] Waganer, L.M., 2013. ARIES cost account documentation. San Diego: University of California.
 
     # 0.5 from lack of Tritium - we don't need so much structure in the containment building.
-    fuel_scaling_factor = 1.0 if inputs.basic.fuel_type == FuelType.DT else 0.5
+    fuel_scaling_factor = get_fuel_scaling_factor(basic.reactor_type, basic.fuel_type)
 
     # 21.01.00,,Site improvements and facs. Source: [1] cost account 13, page 134
     OUT.C210100 = M_USD(k_to_m_usd(268) * p_et * fuel_scaling_factor)
@@ -91,7 +92,7 @@ def cas_21(inputs: AllInputs, data: Data) -> TemplateProvider:
         + OUT.C211700
     )
 
-    if inputs.basic.noak:
+    if basic.noak:
         OUT.C211900 = M_USD(0)
     else:
         OUT.C211900 = M_USD(0.1 * OUT.C210000)  # 10% contingency
@@ -100,24 +101,32 @@ def cas_21(inputs: AllInputs, data: Data) -> TemplateProvider:
 
     OUT.template_file = "CAS210000.tex"
     OUT.replacements = {
-        "C210000": str(round(data.cas21.C210000, 1)),
-        "C210100": str(round(data.cas21.C210100, 1)),
-        "C210200": str(round(data.cas21.C210200, 1)),
-        "C210300": str(round(data.cas21.C210300, 1)),
-        "C210400": str(round(data.cas21.C210400, 1)),
-        "C210500": str(round(data.cas21.C210500, 1)),
-        "C210600": str(round(data.cas21.C210600, 1)),
-        "C210700": str(round(data.cas21.C210700, 1)),
-        "C210800": str(round(data.cas21.C210800, 1)),
-        "C210900": str(round(data.cas21.C210900, 1)),
-        "C211000": str(round(data.cas21.C211000, 1)),
-        "C211100": str(round(data.cas21.C211100, 1)),
-        "C211200": str(round(data.cas21.C211200, 1)),
-        "C211300": str(round(data.cas21.C211300, 1)),
-        "C211400": str(round(data.cas21.C211400, 1)),
-        "C211500": str(round(data.cas21.C211500, 1)),
-        "C211600": str(round(data.cas21.C211600, 1)),
-        "C211700": str(round(data.cas21.C211700, 1)),
-        "C211900": str(round(data.cas21.C211900, 1)),  # TODO - not in the template file
+        "C210000": str(round(OUT.C210000, 1)),
+        "C210100": str(round(OUT.C210100, 1)),
+        "C210200": str(round(OUT.C210200, 1)),
+        "C210300": str(round(OUT.C210300, 1)),
+        "C210400": str(round(OUT.C210400, 1)),
+        "C210500": str(round(OUT.C210500, 1)),
+        "C210600": str(round(OUT.C210600, 1)),
+        "C210700": str(round(OUT.C210700, 1)),
+        "C210800": str(round(OUT.C210800, 1)),
+        "C210900": str(round(OUT.C210900, 1)),
+        "C211000": str(round(OUT.C211000, 1)),
+        "C211100": str(round(OUT.C211100, 1)),
+        "C211200": str(round(OUT.C211200, 1)),
+        "C211300": str(round(OUT.C211300, 1)),
+        "C211400": str(round(OUT.C211400, 1)),
+        "C211500": str(round(OUT.C211500, 1)),
+        "C211600": str(round(OUT.C211600, 1)),
+        "C211700": str(round(OUT.C211700, 1)),
+        "C211900": str(round(OUT.C211900, 1)),  # TODO - not in the template file
     }
     return OUT
+
+
+def get_fuel_scaling_factor(reactor_type: ReactorType, fuel_type: FuelType):
+    if reactor_type != ReactorType.MFE:
+        return 1.0
+    if fuel_type == FuelType.DT:
+        return 1.0
+    return 0.5
--- pyfecons/costing/ife/ife.py
+++ pyfecons/costing/ife/ife.py
@@ -13,7 +13,7 @@ from pyfecons.templates import (
 )
 from pyfecons.costing.ife.PowerBalance import power_balance
 from pyfecons.costing.calculations.cas10_pre_construction import cas_10_pre_construction_costs
-from pyfecons.costing.ife.CAS21 import cas_21
+from pyfecons.costing.calculations.cas21_buildings import cas_21_building_costs
 from pyfecons.costing.ife.cas22.CAS220101 import cas_220101_reactor_equipment
 from pyfecons.costing.ife.cas22.CAS220102 import cas_220102_shield
 from pyfecons.costing.ife.cas22.CAS220103 import cas_220103_lasers
@@ -85,7 +85,7 @@ def GenerateCostingData(inputs: AllInputs) -> CostingData:
     data = Data(reactor_type=ReactorType.IFE)
     data.power_table = power_balance(inputs.basic, inputs.power_input)
     data.cas10 = cas_10_pre_construction_costs(inputs.basic, data.power_table)
-    data.cas21 = cas_21(inputs, data)
+    data.cas21 = cas_21_building_costs(inputs, data)
     data.cas220101 = cas_220101_reactor_equipment(inputs, data)
     data.cas220102 = cas_220102_shield(inputs, data)
     data.cas220103 = cas_220103_lasers(inputs, data)
