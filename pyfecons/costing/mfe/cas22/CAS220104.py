f57c734d5c0233757ff5b5ceaa31e1877dcf7c27
craastad <431915+craastad@users.noreply.github.com>
2025-02-26 14:19:44 +0700

    Create explicit input and output for MFE cas220104

--- pyfecons/costing/mfe/cas22/CAS220104.py
+++ pyfecons/costing/mfe/cas22/CAS220104.py
@@ -1,35 +1,36 @@
-from pyfecons.data import Data, CAS220104SupplementaryHeating
-from pyfecons.report import TemplateProvider
+from pyfecons.data import CAS220104SupplementaryHeating
+from pyfecons.inputs.supplementary_heating import SupplementaryHeating
 from pyfecons.helpers import safe_round
-from pyfecons.inputs.all_inputs import AllInputs
 from pyfecons.units import M_USD
 
 
-def cas_220104_supplementary_heating(inputs: AllInputs, data: Data) -> TemplateProvider:
+def cas_220104_supplementary_heating_costs(
+    supplementary_heating: SupplementaryHeating,
+) -> CAS220104SupplementaryHeating:
     # 22.1.4 Supplementary heating
-    IN = inputs.supplementary_heating
-    OUT: CAS220104SupplementaryHeating = data.cas220104
-    assert isinstance(OUT, CAS220104SupplementaryHeating)
+    heating = supplementary_heating
+    cas220104 = CAS220104SupplementaryHeating()
+    assert isinstance(cas220104, CAS220104SupplementaryHeating)
 
-    OUT.C22010401 = M_USD(IN.average_nbi.cost_2023 * IN.nbi_power)
-    OUT.C22010402 = M_USD(IN.average_icrf.cost_2023 * IN.icrf_power)
-    OUT.C220104 = M_USD(OUT.C22010401 + OUT.C22010402)
+    cas220104.C22010401 = M_USD(heating.average_nbi.cost_2023 * heating.nbi_power)
+    cas220104.C22010402 = M_USD(heating.average_icrf.cost_2023 * heating.icrf_power)
+    cas220104.C220104 = M_USD(cas220104.C22010401 + cas220104.C22010402)
 
     heating_table_rows = "\n".join(
         [
             f"        {ref.name} & {ref.type} & {round(ref.power, 2)} "
             + f"& {safe_round(ref.cost_2009, 2)} & {round(ref.cost_2023, 2)} \\\\"
-            for ref in IN.heating_refs()
+            for ref in heating.heating_refs()
         ]
     )
 
-    OUT.template_file = "CAS220104_MFE_DT.tex"
-    OUT.replacements = {
-        "C22010401": str(round(OUT.C22010401, 3)),
-        "C22010402": str(round(OUT.C22010402, 3)),
-        "C220104__": str(round(OUT.C220104, 3)),
-        "NBIPOWER": str(round(IN.nbi_power, 3)),
-        "ICRFPOWER": str(round(IN.icrf_power, 3)),
+    cas220104.template_file = "CAS220104_MFE_DT.tex"
+    cas220104.replacements = {
+        "C22010401": str(round(cas220104.C22010401, 3)),
+        "C22010402": str(round(cas220104.C22010402, 3)),
+        "C220104__": str(round(cas220104.C220104, 3)),
+        "NBIPOWER": str(round(heating.nbi_power, 3)),
+        "ICRFPOWER": str(round(heating.icrf_power, 3)),
         "HEATING_TABLE_ROWS": heating_table_rows,
     }
-    return OUT
+    return cas220104
--- pyfecons/costing/mfe/mfe.py
+++ pyfecons/costing/mfe/mfe.py
@@ -19,7 +19,7 @@ from pyfecons.costing.calculations.cas22.cas220101_reactor_equipment import (
 )
 from pyfecons.costing.calculations.cas22.cas220102_shield import cas_220102_shield_costs
 from pyfecons.costing.mfe.cas22.cas220103_coils import cas_220103_coils
-from pyfecons.costing.mfe.cas22.CAS220104 import cas_220104_supplementary_heating
+from pyfecons.costing.mfe.cas22.CAS220104 import cas_220104_supplementary_heating_costs
 from pyfecons.costing.mfe.cas22.CAS220105 import cas_220105_primary_structure
 from pyfecons.costing.mfe.cas22.CAS220106 import cas_220106_vacuum_system
 from pyfecons.costing.mfe.cas22.CAS220107 import cas_220107_power_supplies
@@ -100,7 +100,9 @@ def GenerateCostingData(inputs: AllInputs) -> CostingData:
     data.cas220103 = cas_220103_coils(
         inputs.coils, inputs.radial_build, data.power_table
     )
-    data.cas220104 = cas_220104_supplementary_heating(inputs, data)
+    data.cas220104 = cas_220104_supplementary_heating_costs(
+        inputs.supplementary_heating
+    )
     data.cas220105 = cas_220105_primary_structure(inputs, data)
     data.cas220106 = cas_220106_vacuum_system(inputs, data)
     data.cas220107 = cas_220107_power_supplies(inputs, data)
