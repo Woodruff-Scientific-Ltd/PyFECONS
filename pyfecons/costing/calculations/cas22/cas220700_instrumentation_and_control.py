53d3ca97d9b2de8e8f89030bf4eaa4005b127d8c
craastad <431915+craastad@users.noreply.github.com>
2025-03-04 13:14:25 +0700

    Merge mfe & ife CAS220700 calculation.

--- pyfecons/costing/calculations/cas22/cas220700_instrumentation_and_control.py
+++ pyfecons/costing/calculations/cas22/cas220700_instrumentation_and_control.py
@@ -1,4 +1,15 @@
+from pyfecons.data import Data
+from pyfecons.report import TemplateProvider
 from pyfecons.units import M_USD
+
+
+def cas_2207_instrumentation_and_control_costs(data: Data) -> TemplateProvider:
+    # Cost Category 22.7 Instrumentation and Control
+    OUT = data.cas2207
+    OUT.C220700 = compute_instrumentation_and_control_costs()
+    OUT.template_file = "CAS220700.tex"
+    OUT.replacements = {"C220700": round(data.cas2207.C220700)}
+    return OUT
 
 
 def compute_instrumentation_and_control_costs() -> M_USD:
