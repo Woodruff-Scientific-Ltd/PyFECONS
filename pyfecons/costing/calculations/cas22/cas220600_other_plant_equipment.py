436c4aa5ba72aba3eadbf3aa99fd8b9fe243c50e
craastad <431915+craastad@users.noreply.github.com>
2025-03-04 13:08:39 +0700

    Make CAS220600 inputs & output explicit.

--- pyfecons/costing/calculations/cas22/cas220600_other_plant_equipment.py
+++ pyfecons/costing/calculations/cas22/cas220600_other_plant_equipment.py
@@ -1,15 +1,15 @@
-from pyfecons.data import Data
-from pyfecons.report import TemplateProvider
+from pyfecons.costing.accounting.power_table import PowerTable
+from pyfecons.data import CAS2206
 from pyfecons.units import MW, M_USD
 
 
-def cas_2206_other_reactor_plant_equipment_costs(data: Data) -> TemplateProvider:
+def cas_2206_other_reactor_plant_equipment_costs(power_table: PowerTable) -> CAS2206:
     # Cost Category 22.6 Other Reactor Plant Equipment
-    OUT = data.cas2206
-    OUT.C220600 = compute_other_plant_equipment_costs(data.power_table.p_net)
-    OUT.template_file = "CAS220600.tex"
-    OUT.replacements = {"C220600": round(OUT.C220600)}
-    return OUT
+    cas2206 = CAS2206()
+    cas2206.C220600 = compute_other_plant_equipment_costs(power_table.p_net)
+    cas2206.template_file = "CAS220600.tex"
+    cas2206.replacements = {"C220600": round(cas2206.C220600)}
+    return cas2206
 
 
 def compute_other_plant_equipment_costs(p_net: MW) -> M_USD:
