c03b9d71c11256c837b4261f6857c77e9dea2adc
craastad <431915+craastad@users.noreply.github.com>
2025-03-04 12:42:15 +0700

    Merge ife & mfe CAS220600.

--- pyfecons/costing/calculations/cas22/cas220600_other_plant_equipment.py
+++ pyfecons/costing/calculations/cas22/cas220600_other_plant_equipment.py
@@ -1,4 +1,15 @@
+from pyfecons.data import Data
+from pyfecons.report import TemplateProvider
 from pyfecons.units import MW, M_USD
+
+
+def cas_2206_other_reactor_plant_equipment_costs(data: Data) -> TemplateProvider:
+    # Cost Category 22.6 Other Reactor Plant Equipment
+    OUT = data.cas2206
+    OUT.C220600 = compute_other_plant_equipment_costs(data.power_table.p_net)
+    OUT.template_file = "CAS220600.tex"
+    OUT.replacements = {"C220600": round(OUT.C220600)}
+    return OUT
 
 
 def compute_other_plant_equipment_costs(p_net: MW) -> M_USD:
