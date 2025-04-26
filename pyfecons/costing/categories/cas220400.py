8defcad7c240c2be05e1db6bca1065d7579dffa5
craastad <431915+craastad@users.noreply.github.com>
2025-04-26 13:47:52 +0300

    Split CAS2204 calculations and report specification.

--- pyfecons/costing/categories/cas220400.py
+++ pyfecons/costing/categories/cas220400.py
@@ -1,10 +1,9 @@
 from dataclasses import dataclass
 
-from pyfecons.report import ReportSection
 from pyfecons.units import M_USD
 
 
 @dataclass
-class CAS2204(ReportSection):
+class CAS2204:
     # Cost Category 22.4 Radwaste
     C220400: M_USD = None
