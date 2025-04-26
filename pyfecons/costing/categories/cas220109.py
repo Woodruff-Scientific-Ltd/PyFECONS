2cd70c9636f24c2f647050e8dd6a8c92fcf86091
craastad <431915+craastad@users.noreply.github.com>
2025-04-26 13:09:14 +0300

    Split CAS220109 calculations and report specification.

--- pyfecons/costing/categories/cas220109.py
+++ pyfecons/costing/categories/cas220109.py
@@ -1,11 +1,10 @@
 from dataclasses import dataclass
 
-from pyfecons.report import ReportSection
 from pyfecons.units import M_USD
 
 
 @dataclass
-class CAS220109(ReportSection):
+class CAS220109:
     # 22.1.9 Direct Energy Converter
     C220109: M_USD = None
     costs: dict[str, M_USD] = None
