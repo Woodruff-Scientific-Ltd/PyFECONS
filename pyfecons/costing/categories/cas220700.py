ea91989147ad89ef15ec29e220a953e558c24eb3
craastad <431915+craastad@users.noreply.github.com>
2025-04-26 14:03:09 +0300

    Split CAS2207 calculations and report specification.

--- pyfecons/costing/categories/cas220700.py
+++ pyfecons/costing/categories/cas220700.py
@@ -1,10 +1,9 @@
 from dataclasses import dataclass
 
-from pyfecons.report import ReportSection
 from pyfecons.units import M_USD
 
 
 @dataclass
-class CAS2207(ReportSection):
+class CAS2207:
     # Cost Category 22.7 Instrumentation and Control
     C220700: M_USD = None
--- pyfecons/report/utils.py
+++ pyfecons/report/utils.py
@@ -17,6 +17,7 @@ from pyfecons.report.sections.cas220300_section import CAS2203Section
 from pyfecons.report.sections.cas220400_section import CAS2204Section
 from pyfecons.report.sections.cas220500_section import CAS2205Section
 from pyfecons.report.sections.cas220600_section import CAS2206Section
+from pyfecons.report.sections.cas220700_section import CAS2207Section
 from pyfecons.report.sections.power_table_section import PowerTableSection
 from pyfecons.report.sections.cas21_section import CAS21Section
 from pyfecons.report.sections.cas220101_section import CAS220101Section
@@ -64,7 +65,7 @@ def get_report_sections(
         CAS2204Section(costing_data.cas2204),
         CAS2205Section(costing_data.cas2205, inputs.fuel_handling),
         CAS2206Section(costing_data.cas2206),
-        costing_data.cas2207,
+        CAS2207Section(costing_data.cas2207),
         costing_data.cas22,
         costing_data.cas23,
         costing_data.cas24,
